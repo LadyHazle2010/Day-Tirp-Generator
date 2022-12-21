61ef50fddd234454844aeb508f966e33c890e65c
T Jackson <sweetladyhazle@gmail.com>
2022-12-21 11:44:55 -0600

    Updat DayT file

--- DayT.py
+++ DayT.py
@@ -1,36 +1,52 @@
-# destination = ("India", "Columbia", "Peru", "Africa")
-# restaurants = ("olive garden", "gestopo's", "little india", "wakanda forever")
-# transportation = ("airplane", "ship", "train", "tour bus")
-# entertainment = ("jazz concert", "opera", "bar hop", "scubba diving")
+def display_welcome():
+   msg = 'Please choose your trip options!'
+   print(msg)
 
-def random(string):
-        for rand_element_at_index in destination_list:
-           rand_element_at_index = destination_list[0]
+print('Please choose your trip options!')
+
+print('Where would you like to go?') 
+
+list_option = ()
+
+destination = ("India", "Columbia", "Peru", "Africa")
+restaurants = ("olive garden", "gestopo's", "little india", "wakanda forever")
+transportation = ("airplane", "ship", "train", "tour bus")
+entertainment = ("jazz concert", "opera", "bar hop", "scubba diving")
+
+import random
+
+def generate_random_element():
+   return random.randint()
+
+
+for rand_element_at_index in generate_random_element():
+           rand_element_at_index(0,1,2,3) 
+           random.randint()
 print('Where would you like to go?')
 
-destination_list =['India', 'Columbia', 'Peru', 'Africa']
-rand_element_at_index = destination_list[1]
-rand_element_at_index = destination_list[2]
-rand_element_at_index = destination_list[3]
-rand_element_at_index = destination_list[0]
-print()
+list_option =['India', 'Columbia', 'Peru', 'Africa']
+rand_element_at_index = list_option[1]
+rand_element_at_index = list_option[2]
+rand_element_at_index = list_option[3]
+rand_element_at_index = list_option[0]
+print(list_option)
 
-if destination_list is True:
+while list_option is True:
    input()
    destination = False
    print()
 
-elif destination_list is True:
+if list_option is True:
    input()
    destination = False
-   print(destination_list)
+   print(list_option)
 
-elif destination_list is True:
+elif list_option is True:
    input()
    # destination = False
-   print(destination_list)
+   print(list_option)
 
-elif destination_list is True:
+elif list_option is True:
    input()
    # destination = False
-   print(destination_list)
+   print(list_option)
