bc2a4e507ae7067e0563b2f9a104e4a389d9727a
T Jackson <sweetladyhazle@gmail.com>
2022-12-20 22:24:29 -0600

    Updated DayT file

--- DayT.py
+++ DayT.py
@@ -1,2 +1,70 @@
-print("hello world")
-hi
+def get_destination_name():
+    destination_choice = ('Please enter your destiantion choice.')
+    print(destination_choice)
+
+
+
+def get_destination_name("India," " Columbia," "Peru," "Africa,"):
+    destination_choice = ("Where would you like to go?")
+    print(destination_choice)
+
+    get_destination_name()
+
+
+
+
+# restaurants = ("olive garden", "gestopo's", "little india", "wakanda forever")
+# transportation = ("airplane", "ship", "train", "tour bus")
+# entertainment = ("jazz concert", "opera", "bar hop", "scubba diving")
+
+# #Get user input and print
+
+
+
+
+
+
+ 
+print('Where would you like to go?')
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def random(string):
+#    for destination in string:
+#       final_result = " "
+#       print(destination)
+
+#    if destination == ("New York"):
+#       print(final_result)
+
+#    elif destination == ("Columbia"):
+#       print(final_result)
+
+#    elif destination == ("Peru"):
+#       print(final_result)
+
+#    elif destination == ("Africa"):
+#       print(final_result)
+
+#    else: 
+#       destination != (" ")
+#       print("Not applicable")
+
+
+
+
+
+
+# def problem3(string):
+#     for char in string:
+#         print(char)
