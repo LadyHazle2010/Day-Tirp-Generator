91d124dea43da407210f49adee5199de59df1672
T Jackson <sweetladyhazle@gmail.com>
2022-12-21 02:03:09 -0600

    update DayT file

--- DayT.py
+++ DayT.py
@@ -1,150 +1,49 @@
-
-destination_list =['India', 'Columbia', 'Peru', 'Africa']
-
-def destination_name():
-    destination = input("India", "Columbia", "Peru", "Africa ")
-    print(destination)
-
-    def random(string):
-        for destination_list in string:
-         final_result = " "
-print(destination_list)
-
-if destination_list == (" "):
-      print()
-
-elif destination_list == (" "):
-      print(destination_list)
-
-elif destination_list == (" "):
-      print(destination_list)
-
-elif destination_list == (" "):
-      print(destination_list)
-
-else: 
-      destination_list != (" ")
-      print("Not applicable")
-
-destination_list is True
-if input():
-    destination = False
-    print()
-print('Where would you like to go?')
-
-destination is True
-if input():
-    destination = False
-    print(destination)
-print('Where would you like to go?')
-
-destination is True
-if input():
-    destination =False
-    print(destination)
-print('Where would you like to go?')
-
-destination is True 
-if input():
- destination = False
-print(destination)
-print('Where would you like to go?')
-
-
-
-
-# def destination_choice = input("Where would you like to go?")
-# print(destination_choice)
-
-# destination_choice()
-
-
-
 # destination = ("India", "Columbia", "Peru", "Africa")
 # restaurants = ("olive garden", "gestopo's", "little india", "wakanda forever")
 # transportation = ("airplane", "ship", "train", "tour bus")
 # entertainment = ("jazz concert", "opera", "bar hop", "scubba diving")
 
-# #Get user input and print
+def random(string):
+        for char in destination_list:
+           rand_element_at_index = destination_list[0]
+print('Where would you like to go?')
 
+destination_list =['India', 'Columbia', 'Peru', 'Africa']
+rand_element_at_index = destination_list[1]
+rand_element_at_index = destination_list[2]
+rand_element_at_index = destination_list[3]
+rand_element_at_index = destination_list[0]
+print(rand_element_at_index)
 
+if destination_list is True:
+   input()
+   destination = False
+   print()
 
+elif destination_list is True:
+   input()
+   destination = False
+   print(destination_list)
 
+elif destination_list is True:
+   input()
+   # destination = False
+   print(destination_list)
 
+elif destination_list is True:
+   input()
+   # destination = False
+   print(destination_list)
 
-# def problem3(string):
-#     for char in string:
-#         print(char)
-
-
-# def get_destination_name():
-#     destination_choice = ('Please enter your destiantion choice.')
-#     print(destination_choice)
-
-
-
-# def get_destination_name("India," " Columbia," "Peru," "Africa,"):
-#     destination_choice = ("Where would you like to go?")
-#     print(destination_choice)
-
-#     get_destination_name()
-
-
-
-
-# restaurants = ("olive garden", "gestopo's", "little india", "wakanda forever")
-# transportation = ("airplane", "ship", "train", "tour bus")
-# entertainment = ("jazz concert", "opera", "bar hop", "scubba diving")
-
-# #Get user input and print
-
-
-
-# def destination():
-#    print_result = (input) 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# def random(string):
-#    for destination in string:
-#       final_result = " "
-#       print(destination)
-
-#    if destination == ("New York"):
-#       print(final_result)
-
-#    elif destination == ("Columbia"):
-#       print(final_result)
-
-#    elif destination == ("Peru"):
-#       print(final_result)
-
-#    elif destination == ("Africa"):
-#       print(final_result)
-
-#    else: 
-#       destination != (" ")
-#       print("Not applicable")
-
-
-
-
-
+# list_name = ['javaScript', 'Python', 'Java', 'Django', 'React']
+# element_at_second_index = list_name[1]
+# print(element_at_second_index)
 
 # def problem3(string):
 #     for char in string:
 #         print(char)
+
+# def destination_choice = input("Where would you like to go?")
+# print(destination_choice)
+
+# destination_choice()
